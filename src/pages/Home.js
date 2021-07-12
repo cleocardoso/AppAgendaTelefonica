815519670b2046e55f9d63e5651d21fc7d14cccb
cleocardoso <cleotads21@gmail.com>
2021-07-12 13:08:00 -0300

    Adicionando AsyncStorage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,62 +1,117 @@
-import React, { useState } from 'react';
-import { Text, View,StyleSheet,TouchableOpacity,FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View,StyleSheet,TouchableOpacity,FlatList, Alert, Keyboard} from 'react-native';
 import { MyInput } from '../components/MyInput';
+import  ItemContact  from '../components/ItemContact';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export function Home(){
-   
-    const [user,setUser] = useState('');
-    const [phone,setPhone] = useState('');
-   
 
-   
+export function Home() {
 
-    return(
-        <View style={ styles.container}>
-            <View style={ styles.head }>
+    const keyAsyncStorage = "@agenda:contatos";
+
+    const [user, setUser] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [contacts, setContacts] = useState([]);
+
+    async function salvarContato() {
+        const data = {
+            id: String(new Date().getTime()),
+            name: user,
+            telefone: telefone
+        }
+        const vetData = [...contacts, data];
+
+        try {
+            await AsyncStorage.setItem(keyAsyncStorage, JSON.stringify(vetData));
+        } catch (error) {
+            Alert.alert("Erro ao salvar contatos");
+        }
+
+        Keyboard.dismiss();
+        setUser("");
+        setTelefone("");
+        loadData();
+        
+    }
+    async function deletarContato( id ) {
+        const newData = contacts.filter( item => item.id != id );
+        await AsyncStorage.setItem(keyAsyncStorage, JSON.stringify( newData ));
+        
+        setContacts(newData); 
+    }
+
+    async function loadData(){
+        try{
+            const retorno = await AsyncStorage.getItem(  keyAsyncStorage  );   
+            const dadosContacts = JSON.parse( retorno )
+            console.log( dadosContacts );
+            setContacts( dadosContacts || [] );
+        }catch(error){
+            Alert.alert("Erro na leitura  dos contatos");
+        }
+    }
+
+    useEffect( ()=>{
+        loadData();      
+    } , []);
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.head}>
                 <Text style={styles.titlehead}>AGENDA TELEFÔNICA</Text>
             </View>
-            
+            {/*onChangeText: funcao para retorna o valor do input*/}
             <View style={styles.formContainer}>
-                <MyInput iconName="user" textInput="Nome" value={user} onChangeText={setUser} /> 
-                <MyInput iconName="phone" textInput="Telefone" value={phone} onChangeText={setPhone} keyboardType="numeric" /> 
-                
-                <MyInput/>
+                <MyInput iconName="user" textInput="Nome" value={user} onChangeText={(e) => setUser(e)} />
+                <MyInput iconName="phone" textInput="Telefone" value={telefone} onChangeText={(e) => setTelefone(e)} keyboardType="numeric" />
 
-                <TouchableOpacity style={ styles.button }>
+
+
+                <TouchableOpacity style={styles.button} onPress={salvarContato}>
                     <Text style={styles.buttonText}>Salvar</Text>
                 </TouchableOpacity>
             </View>
 
-          
-           
-            
+            <View style={styles.list}>
+                <Text style={styles.titleList}>Meus Contatos</Text>
+                
+                <FlatList data={contacts}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <ItemContact nome={item.name} telefone={item.telefone} apagar={() => deletarContato(item.id)} />
+                    )}
+                />
+            </View>
+
+
         </View>
 
     );
-    
+
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         paddingVertical: 40,
         alignItems: 'center',
     },
-    head:{
+    head: {
         width: '100%',
         height: 35,
-        marginTop:-40,
+        marginTop: -40,
         backgroundColor: '#613EEA',
         alignItems: 'center',
         justifyContent: 'center'
     },
-    titlehead:{
+    titlehead: {
         fontSize: 20,
         fontWeight: 'bold',
         color: '#FFFFFF',
-        
+
     },
-    formContainer:{
+    formContainer: {
         width: "85%",
         marginTop: 30,
         alignItems: 'center',
@@ -70,14 +125,21 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginTop: 20,
         width: 300,
-        
+
     },
     buttonText: {
         color: '#FFF',
         fontSize: 17,
         fontWeight: 'bold',
     },
-    
-    
+    list: {
+        width: "90%",
+    },
+    titleList: {
+        marginTop: 40,
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
 
 });
